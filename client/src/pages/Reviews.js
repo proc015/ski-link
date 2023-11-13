136536f46833fbe29134d7e4d727cb0bede46d16
proc015 <proc015@gmail.com>
2023-11-13 13:33:53 +0100

    feat: added star component

--- client/src/pages/Reviews.js
+++ client/src/pages/Reviews.js
@@ -1,5 +1,6 @@
 import AddReview from "../components/AddReview"
 import NavBar from "../components/NavBar"
+import StarRating from "../components/StarRating"
 
 
 const Reviews = () => {
@@ -7,6 +8,7 @@ const Reviews = () => {
     <div>
       
       <AddReview />
+      <StarRating />
     </div>
   )
 }
