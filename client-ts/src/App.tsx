9a7de4e2a4f89719ce6a0cc22cf3a18c4e78e28f
Zarina Saidova Poyet <zarina.poyet@gmail.com>
2023-11-17 12:20:06 +0100

    feat: routes done

--- client-ts/src/App.tsx
+++ client-ts/src/App.tsx
@@ -1,34 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link } from "react-router-dom";
+import Home from './pages/Home';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import ReviewList from './components/ReviewList';
 
 import homeImage from "./images/homepage_img.png";
+import Client from './pages/Client';
+import Dashboard from './pages/Dashboard';
+import Instructor from './pages/Instructor';
+import Login from './pages/Login';
+import Review from './pages/Review';
+import SkiLesson from './pages/SkiLesson';
 
 function App() {
   return (
-    <div className="full-app-container">
-      <NavBar />
-
-<div className="home-body-container">
-  <img id="home-image" src={homeImage} alt="SkiLink Logo"></img>
-
-  <div className="home-text-container">
-    <h1> Ascend to new </h1>
-    <p id="peaks"> peaks </p>
-    <p id="home-tagline"> Your alpine journey starts here </p>
-
-    <button id="home-booking">
-      <Link to="/ski-lessons">Book Ski Lessons</Link>
-    </button>
-  </div>
-</div>
-
-<ReviewList />
-
-
-<Footer />
+    <div className="app-container">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/client' element={<Client />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/instructor' element={<Instructor />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/review' element={<Review />} />
+          <Route path='/skilesson' element={<SkiLesson />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ client-ts/src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import Footer from '../components/Footer';
+import NavBar from '../components/NavBar';
+import ReviewList from '../components/ReviewList';
+
+import homeImage from "./images/homepage_img.png";
+
+const Home = () => {
+    return (
+        <div className="full-app-container">
+            <NavBar />
+
+            <div className="home-body-container">
+                <img id="home-image" src={homeImage} alt="SkiLink Logo"></img>
+
+                <div className="home-text-container">
+                    <h1> Ascend to new </h1>
+                    <p id="peaks"> peaks </p>
+                    <p id="home-tagline"> Your alpine journey starts here </p>
+
+                    <button id="home-booking">
+                        <Link to="/ski-lessons">Book Ski Lessons</Link>
+                    </button>
+                </div>
+            </div>
+
+            <ReviewList />
+
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Home
