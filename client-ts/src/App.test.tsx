8658bc7be8887418e98ce1f813b3f485ee25174a
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-11-20 13:19:51 +0100

    added test App render and Login

--- client-ts/src/App.test.tsx
+++ client-ts/src/App.test.tsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import Login from './pages/Login';
+
+jest.mock('swiper/react', () => ({
+  Swiper: ({ children }: { children?: ReactNode }) => <div>{children}</div>,
+  SwiperSlide: ({ children }: { children?: ReactNode }) => <div>{children}</div>,
+}));
+
+jest.mock('swiper/modules', () => ({
+  Navigation: () => null
+}));
+
+jest.mock('swiper/css', () => jest.fn());
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Login/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render login', () => {
+  render(
+    <MemoryRouter>
+      <Login />
+    </MemoryRouter>
+  );
+
+  const getLogin = screen.getByText(/Login/i);
+  const getRegister = screen.getByText(/Register/i);
+
+  expect(getLogin).toBeInTheDocument();
+  expect(getRegister).toBeInTheDocument();
+});
+
+
